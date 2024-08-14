a4a679265f01f659a5f163712f0fa5ff3a9b4670
Magali-Cristobo <alumno21.cristobo.magali@ipm.edu.ar>
2024-08-14 20:51:23 -0300

    nav

--- nombreApp/src/app/app.routes.ts
+++ nombreApp/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:"", component:InicioComponent}, {path:"login", component:LoginComponent}];
